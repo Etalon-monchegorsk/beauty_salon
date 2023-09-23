f63351b8ba8fd944418865d82e013d6896c2bb4c
Etalon-monchegorsk <bitnit633@gmail.com>
2023-09-23 16:01:38 +0300

    Allwork

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const swiper = new Swiper('.swiper-master', {
 
   // Optional parameters
@@ -25,73 +25,4 @@ const swiper = new Swiper('.swiper-master', {
     }
   }
 
-  // If we need pagination
-  // pagination: {
-  //    el: '.swiper-pagination',
-  // },
-
-  //  // Navigation arrows
-  // navigation: {
-  //    nextEl: '.swiper-button-next',
-  //    prevEl: '.swiper-button-prev',
-  // },
-
-  //  // And if we need scrollbar
-  // scrollbar: {
-  //    el: '.swiper-scrollbar',
-  // },
-});
-const priceSlider = new Swiper('.price__slider', {
-  loop: false,
-  slidesPerView: 1,
-  spaceBetween: 80,
-  speed: 1000,
-  effect: 'slide',
-  parallax: true,
-  navigation: {
-    prevEl: '.price .slider-prev',
-    nextEl: '.price .slider-next',
-  },
-=======
-const swiper = new Swiper('.swiper-master', {
-
-  // Optional parameters
-  loop: false,
-  slidesPerView: 3,
-  spaceBetween: 80,
-  speed: 1000,
-  navigation: {
-    prevEl: '.master .slider-prev',
-    nextEl: '.master .slider-next',
-  },
-  
-
-  // If we need pagination
-  // pagination: {
-  //    el: '.swiper-pagination',
-  // },
-
-  //  // Navigation arrows
-  // navigation: {
-  //    nextEl: '.swiper-button-next',
-  //    prevEl: '.swiper-button-prev',
-  // },
-
-  //  // And if we need scrollbar
-  // scrollbar: {
-  //    el: '.swiper-scrollbar',
-  // },
-});
-const priceSlider = new Swiper('.price__slider', {
-  loop: false,
-  slidesPerView: 1,
-  spaceBetween: 80,
-  speed: 1000,
-  effect: 'slide',
-  parallax: true,
-  navigation: {
-    prevEl: '.price .slider-prev',
-    nextEl: '.price .slider-next',
-  },
->>>>>>> e127a7435aba2c9f8ccb4c7375d221665d717750
-});
+})
