34ddf55f7bb994a6f0e1b0c1326f83ab65de6dfa
Etalon-monchegorsk <bitnit633@gmail.com>
2023-09-30 15:24:48 +0300

    Etalon

--- js/slider.js
+++ js/slider.js
@@ -25,4 +25,32 @@ const swiper = new Swiper('.swiper-master', {
     }
   }
 
+});
+const sliderPrice = new Swiper('.price__slider', {
+
+  // Optional parameters
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 80,
+  parallax: true,
+  speed: 1000,
+  navigation: {
+    prevEl: '.price .slider-prev',
+    nextEl: '.price .slider-next',
+  },
+  // breakpoints: {
+  //   320: {
+  //     spaceBetween: 10,
+  //     slidesPerView: 1,
+  //   },
+  //   660: {
+  //     spaceBetween: 20,
+  //     slidesPerView: 2,
+  //   },
+  //   1000: {
+  //     spaceBetween: 20,
+  //     slidesPerView: 3
+  //   }
+  // }
+
 })
